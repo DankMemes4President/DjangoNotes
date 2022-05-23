e4d36bb4f45382f5238902d4d4f651fabb66abc8
Harsh Singh <43901843+DankMemes4President@users.noreply.github.com>
2022-05-24 00:07:16 +0530

    Defined basic models in models.py

--- Notes/models.py
+++ Notes/models.py
@@ -1,1 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Note(models.Model):
+    title = models.CharField(max_length=500)
+    content = models.TextField()
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=100)
+    note = models.ForeignKey(Note, on_delete=models.CASCADE)
